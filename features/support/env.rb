e3c9fd5b4f4e6305ed8f130142adfe2883178c27
Naresh Sekar <naresh.sekar@weinvest.net>
2019-12-01 12:01:32 +0530

    adding global data and allure

---
+++ features/support/env.rb
@@ -0,0 +1,38 @@
+require 'allure-cucumber'
+require 'webdriver_manager'
+require 'cucumber'
+require 'require_all'
+require 'pathname'
+require 'fileutils'
+require "pry"
+require 'pry-remote'
+require_rel "../locators/"
+require_rel "../libraries/"
+require_rel "../pages/"
+require "time"
+require 'faker'
+require 'git'
+require 'rest-client'
+require 'json'
+
+include AllureCucumber::DSL
+
+if File.exist?('features/global-data/global.yml')
+    $conf =  YAML.load_file('features/global-data/global.yml')
+else
+    puts "features/global-data/global.yml is not found !!!"
+end
+
+AllureCucumber::configure do |c|
+    c.output_dir = "reports/allure"
+    c.clean_dir  = false
+end
+
+Cucumber::Core::Test::Step.module_eval do
+    def name
+        return text if self.text == 'Before hook'
+        return text if self.text == 'After hook'
+        "#{source.last.keyword}#{text}"
+    end
+end
+
